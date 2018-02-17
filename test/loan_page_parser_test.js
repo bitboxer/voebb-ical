d04b76435719a1cccd96721c340034d64197f61d
Bodo Tasche <bodo@wannawork.de>
2018-02-17 22:31:48 +0100

    parse loan page

--- test/loan_page_parser_test.js
+++ test/loan_page_parser_test.js
@@ -1,10 +1,23 @@
+import fs from 'fs';
+import { expect } from 'chai';
+
 import loanPageParser from '../src/loan_page_parser';
 
-const expect = require('chai').expect;
+describe('LoanPageParser', () => {
 
-describe('First test', () => {
-  it('Should assert true to be true', () => {
-    console.log(loanPageParser());
-    expect(true).to.be.true;
+  it('should return two books', () => {
+    let html = fs.readFileSync('./test/data/loans.html').toString();
+    let result = loanPageParser(html);
+    expect(result).to.eql([{
+        book: "Geisel : [Comic] / Guy Delisle ; [aus dem Französischem von Heike Drescher ; Textbearbeitung: Ulrich Pröfrock]\nComic Deli\n03825165458",
+        date: new Date(2018, 2, 3),
+        location: "Friedrichshain - Kreuzberg: Bibliothek Frankfurter Allee 14a"
+      }, {
+        book: "Aufzeichnungen aus Birma : [Comic] / Guy Delisle. [Aus dem Franz. von Kai Wilksen]\nComic Deli\n02184346458",
+        date: new Date(2018, 2, 3),
+        location: "Friedrichshain - Kreuzberg: Bibliothek Frankfurter Allee 14a"
+      }
+    ]);
   });
+
 });
