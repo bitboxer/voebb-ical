1a18fc6bf25f1ab489171450e4a65a6a2bdc3c8e
Bodo Tasche <bodo@wannawork.de>
2018-02-23 08:06:11 +0100

    mark test as pending

--- test/fetch_loan_page_test.js
+++ test/fetch_loan_page_test.js
@@ -5,7 +5,7 @@ import fetchLoanPage from '../src/fetch_loan_page';
 
 describe('FetchLoanPage', () => {
 
-  it('should load the loan page of an user', async () => {
+  xit('should load the loan page of an user', async () => {
     // TODO: find out how to test this properly
     // const result = await fetchLoanPage(process.env.VOEBB_ID, process.env.VOEBB_PASSWORD);
   }).timeout(60000);
