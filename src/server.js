f77f200cf95d5c8f0cb678d99d2df904c3b18dd6
Bodo Tasche <bodo@wannawork.de>
2018-02-23 08:23:26 +0100

    working server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
-const Koa = require('koa');
+import Koa from 'koa';
+
+import fetchLoanPage from './fetch_loan_page'
+import loanPageParser from './loan_page_parser';
+import ical from './ical';
+
 const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
-  ctx.body = 'Hello World2';
+  const loanPage = await fetchLoanPage(process.env.VOEBB_ID, process.env.VOEBB_PASSWORD);
+  const books = loanPageParser(loanPage);
+  const ics = ical(books);
+
+  ctx.body = ics;
 });
 
-if (!module.parent) app.listen(4000);
+app.listen(4000);
